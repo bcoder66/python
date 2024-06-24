01f64d328652dc7fbd67954c25dc5e234c9702a9
Erla Nithin Reddy <142092640+bcoder66@users.noreply.github.com>
2024-06-24 13:19:27 +0530

    Add files via upload

---
+++ Batting_order.py
@@ -0,0 +1,21 @@
+from random import choice
+print('enter number of players')
+n=int(input())
+l1=[]
+for i in range(n):
+    name=input()
+    l1.append(name)
+l2=[]
+l3=[]
+for i in range(1,(len(l1)+1)):
+    l2.append(i)
+
+for i in range(len(l2)):
+               k=choice(l2)
+               l3.append(k)
+               l2.remove(k)
+z=zip(l1,l3)
+result=sorted(z,key=lambda x:x[1])
+print('batting order:')
+for (a,b) in result:
+    print(a,b)
